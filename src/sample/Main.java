d93ca0f6a7767ca7193826ca4a10df4d59088468
Patrick Watzinger <patrick.watzinger@htl-wels.at>
2021-01-14 09:24:58 +0100

    SchiffeVersenken_version_0.3 Überarbeitung xml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Schiffe Versenken ©OneCentProduction");
-        primaryStage.setScene(new Scene(root, 800, 450));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
