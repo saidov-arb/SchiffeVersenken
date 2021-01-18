10aa459d674188a8e2db584bb54c11b59c3cf575
Jezidzic Marko <marko.jezidzic@htl-wels.at>
2021-01-18 11:26:18 +0100

    SchiffeVersenken_version1.3 AbnormaleDynamic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,11 +15,24 @@ public class Controller implements Initializable
     @FXML TextField txt_name,txt_shipSize;
     @FXML Label lbl_score,lbl_maxShips;
     @FXML Button btn_ready,btn_surrender;
-    @FXML Button btn_h,btn_v;;
+    @FXML Button btn_h,btn_v;
+    @FXML GridPane GP_Feld;
+    Button [][] fireldButtons=new Button[Game.FIRELDSIZE][Game.FIRELDSIZE];
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
-
+        int idCounter=0;
+        for (int i = 0; i < Game.FIRELDSIZE; i++) {
+            for (int j = 0; j < Game.FIRELDSIZE; j++) {
+                fireldButtons[i][j]=new Button("");
+                fireldButtons[i][j].setMinWidth(50);
+                fireldButtons[i][j].setMinHeight(50);
+                fireldButtons[i][j].setStyle("-fx-border-color: black;");
+                fireldButtons[i][j].setId(idCounter+"");
+                idCounter++;
+                this.GP_Feld.add(fireldButtons[i][j],i+3,j);
+            }
+        }
     }
 }
