f2d7ef177e940371a8fb07607e38eb89a4ed4722
Patrick Watzinger <patrick.watzinger@htl-wels.at>
2021-01-18 10:48:02 +0100

    SchiffeVersenken_version1.2 neues Design

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,7 +14,7 @@ public class Controller implements Initializable
     @FXML TextField txt_name,txt_shipSize;
     @FXML Label lbl_score,lbl_maxShips;
     @FXML Button btn_ready,btn_surrender;
-    @FXML Button btn_h,btn_v;
+    @FXML Button btn_h,btn_v;;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
